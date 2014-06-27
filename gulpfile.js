0c1a5a0f1d6b00fb322a103f0b7346754177812d
David Hellsing <david@aino.se>
2014-06-27 20:48:55 +0200

    buildz

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var source = require('vinyl-source-stream')
 var buffer = require('gulp-buffer')
 var concat = require('gulp-concat')
 var path = require('path')
+var uglify = require('gulp-uglify')
+var moment = require('moment')
 
 var modules = ['animate', 'easing', 'finger', 'requestframe']
 
@@ -30,19 +32,42 @@ var gulpBrowserify = function(options, bundleOptions, commands) {
 modules.forEach(function(module) {
   gulp.task(module, function() {
     gutil.log('Building '+module)
+    var upper = module.charAt(0).toUpperCase() + module.substr(1)
     var src = path.join('../', 'ainojs-'+module, module+'.js')
     gulpBrowserify({
       entries: src
     },{
       debug: false,
-      standalone: module.charAt(0).toUpperCase() + module.substr(1)
+      standalone: upper
     })
     .on('error', function(trace) {
       console.error(trace)
     })
     .pipe(source())
     .pipe(buffer())
-    .pipe(concat(module+'.js'))
+    .pipe(uglify())
+    .pipe(map(function(data, file) {
+      var script = data.toString()
+      var header = ''
+      var pakg = fs.readFileSync(path.join('../', 'ainojs-'+module, 'package.json'))
+      if ( pakg ) {
+        pakg = JSON.parse(pakg.toString())
+        var v = parseInt(pakg.version.toString().replace(/\./g,''), 10) + 1
+        v = v.toString().split('')
+        v.splice(-1, 0, '.')
+        v.splice(-3, 0, '.')
+        v = 'v'+v.join('')
+        var comments = [
+          pakg.name + ' ' + v,
+          moment().format("MMMM Do YYYY"),
+          '(c) Aino',
+          'MIT Licensed'
+        ]
+        header = '/* '+comments.join(' - ') + ' */\n'
+      }
+      return header+script
+    }))
+    .pipe(concat(module+'.min.js'))
     .pipe(gulp.dest(path.join('../', 'ainojs-'+module, 'dist')))
   })
 })
