cf436c7799a80e7409b3782b242ebc83dc8b7b55
David Hellsing <david@aino.se>
2015-02-03 11:26:48 +0100

    Building patch from version v1.0.5-1-g8e8f0e7

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ modules.forEach(function(module) {
       .on('error', function(trace) {
         console.error(trace)
       })
-      .pipe(source())
+      .pipe(source(src))
       .pipe(buffer())
       .pipe(uglify())
       .pipe(map(function(data, file) {
